306d590d463fd3f8fd7570900eecb37cb908cecd
Andrey Nepshekuev <nepshekuev.a.88@gmail.com>
2021-03-15 13:56:14 +0200

    before configing stp and bgp

--- device_configuration/devices_config.py
+++ device_configuration/devices_config.py
@@ -4,6 +4,8 @@ from nornir.plugins.functions.text import print_title, print_result
 
 nr = InitNornir(config_file="config.yaml", dry_run=True)
 
+
+# TEST FUCNCTION
 def hostname_config(task):
     var = task.run(task=text.template_file,
                    name="Switches hostname configuration",
@@ -16,28 +18,37 @@ def hostname_config(task):
     name="Configure hostname on the device",
     config_commands=task.host["hostname_config"],
     )
-def interfaces_config(task):
+
+
+# PRODUCTION FUCNCTION
+def nexus_config(task):
     var = task.run(task=text.template_file,
                    name="Switches interface configuration",
-                   template="interfaces.j2",
+                   template="nexus_config_template.j2",
                    # current directory:
                    path="templates")
-    task.host["interfaces_config"] = var.result
+    task.host["nexus_config"] = var.result
     task.run(
     networking.netmiko_send_config, 
     name="Loading Configuration on the device",
-    config_commands=task.host["interfaces_config"].splitlines()
+    config_commands=task.host["nexus_config"].splitlines()
     )
+
+
+# TEST FUCNCTION
 def send_ping(task):
     task.run(
     task=networking.netmiko_send_command, delay_factor=2, max_loops=500,
     command_string='ping 255.255.255.255',
     name="Sending Ping to the device"
     )
+
+
 def main():
     print_title("Playbook to configure the network")
-    task = nr.run(task=hostname_config)
+    task = nr.run(task=nexus_config)
     print_result(task)
+
 
 if __name__ == "__main__":
     main()
